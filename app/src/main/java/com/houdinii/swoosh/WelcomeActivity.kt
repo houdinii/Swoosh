bd40b9a9c1d601efecf7737c2c82306c8027f49b
Brian Barnes <houdinii1984@gmail.com>
2021-10-31 22:54:47 -0600

    lesson 1

--- app/src/main/java/com/houdinii/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/houdinii/swoosh/WelcomeActivity.kt
@@ -2,11 +2,13 @@ package com.houdinii.swoosh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.colorPrimaryDark)
     }
 }
 
