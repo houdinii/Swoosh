77e9071a9cfd9cd3f6cf045f2a9f2614614bc75b
Brian Barnes <houdinii1984@gmail.com>
2021-10-31 22:05:38 -0600

    Initial commit

---
+++ app/src/main/java/com/houdinii/swoosh/WelcomeActivity.kt
@@ -0,0 +1,41 @@
+package com.houdinii.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
+
+/*
+package com.houdinii.devprofile
+
+import android.graphics.BitmapFactory
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import com.houdinii.devprofile.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // Examples:
+        // binding.name.text = viewModel.name
+        // binding.button.setOnClickListener { viewModel.userClicked() }
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius = 15f
+        binding.logo.setImageDrawable(rounded)
+    }
+}
+*/
