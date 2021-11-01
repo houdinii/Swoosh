86eae90762722a51d12cd5f65a641710997fc3fa
Brian Barnes <houdinii1984@gmail.com>
2021-11-01 16:38:34 -0600

    lesson 3

--- app/src/main/java/com/houdinii/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/houdinii/swoosh/WelcomeActivity.kt
@@ -1,43 +1,25 @@
 package com.houdinii.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.ContextCompat
+import com.houdinii.swoosh.databinding.ActivityWelcomeBinding
+
+private lateinit var binding: ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
         window.statusBarColor = ContextCompat.getColor(this, R.color.colorPrimaryDark)
-    }
-}
-
-/*
-package com.houdinii.devprofile
-
-import android.graphics.BitmapFactory
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
-import com.houdinii.devprofile.databinding.ActivityMainBinding
-
-private lateinit var binding: ActivityMainBinding
-
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        // Examples:
-        // binding.name.text = viewModel.name
-        // binding.button.setOnClickListener { viewModel.userClicked() }
-
-        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
-        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
-        rounded.cornerRadius = 15f
-        binding.logo.setImageDrawable(rounded)
+        binding.getStartedBtn.setOnClickListener{
+            // 'this' is the context for 'this' activity
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
-*/
