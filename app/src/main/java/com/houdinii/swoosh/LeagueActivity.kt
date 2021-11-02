d73e7a1a2cdc5ffaa4fda97a0abffb452b30b0bf
Brian Barnes <houdinii1984@gmail.com>
2021-11-01 18:59:21 -0600

    lesson 5

--- app/src/main/java/com/houdinii/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/houdinii/swoosh/LeagueActivity.kt
@@ -1,7 +1,9 @@
 package com.houdinii.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.core.content.ContextCompat
 
 class LeagueActivity : BaseActivity() {
@@ -9,5 +11,10 @@ class LeagueActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
         window.statusBarColor = ContextCompat.getColor(this, R.color.colorPrimaryDark)
+    }
+
+    fun leagueNextClicked(view: View){
+        val skillActivity = Intent(this, SkillActivity::class.java)
+        startActivity(skillActivity)
     }
 }
